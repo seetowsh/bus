1b7dac9236eb6adde5de4973ee2f9215a2f5d465
Hi1307 <seetow.2@gmail.com>
2025-07-29 17:23:45 +0800

    Init OneService

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,5 +1,7 @@
 // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
+importScripts('/OneSignalSDKWorker.js');
+
 const CACHE = "pwabuilder-offline-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
